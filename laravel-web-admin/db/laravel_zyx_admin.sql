b3bc47c35fb69961eac0a29e0c9d68fef98e7667
root <root@DESKTOP-9JRGICS.localdomain>
2019-09-27 17:32:38 +0800

    :speech_balloon: fix db.sql

--- laravel-web-admin/db/laravel_zyx_admin.sql
+++ laravel-web-admin/db/laravel_zyx_admin.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 27/09/2019 09:08:46
+ Date: 27/09/2019 17:29:24
 */
 
 SET NAMES utf8mb4;
@@ -39,6 +39,39 @@ INSERT INTO `admin_accounts` VALUES (9, '222', '$2y$10$oz34mRKLFtodCblrVOe.IedkI
 INSERT INTO `admin_accounts` VALUES (10, '333', '$2y$10$zehWL0mzABG3k9jIF1Wzi.lL8KQ2GbHwaqHvYjfXxX.nuSNoZZcVi', NULL, '2019-09-17 10:26:05', '2019-09-17 10:26:05');
 INSERT INTO `admin_accounts` VALUES (11, '44', '$2y$10$mchCvtG4hvJeLFpsvAD5meVdgSBtnyn2GgAggWTcpeMQ9zgnpp4e.', NULL, '2019-09-17 10:26:53', '2019-09-17 10:26:53');
 INSERT INTO `admin_accounts` VALUES (12, '666', '$2y$10$9pIwHJXDYRLzyN.qKvZNK.VUHmhC3Np1A.uUv6VJD2ZkZ/6gjtGxi', NULL, '2019-09-17 10:31:09', '2019-09-17 10:31:09');
+
+-- ----------------------------
+-- Table structure for admin_photos
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_photos`;
+CREATE TABLE `admin_photos`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片标题',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片路径',
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片标签',
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin_photos
+-- ----------------------------
+INSERT INTO `admin_photos` VALUES (1, 'CH', 'assets/images/gallery/our-gallery-1.jpg', 'massage beauty', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (2, 'CH', 'assets/images/gallery/our-gallery-2.jpg', 'massage beauty', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (3, 'CH', 'assets/images/gallery/our-gallery-3.jpg', 'massage', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (4, 'SJ', 'assets/images/gallery/our-gallery-4.jpg', 'skin-care', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (5, 'SJ', 'assets/images/gallery/our-gallery-5.jpg', 'skin-care beauty', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (6, 'SJ', 'assets/images/gallery/our-gallery-6.jpg', 'skin-care', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (7, 'CDD', 'assets/images/gallery/our-gallery-7.jpg', 'make-hair', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (8, 'CDD', 'assets/images/gallery/our-gallery-8.jpg', 'make-hair', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (9, 'CDD', 'assets/images/gallery/our-gallery-9.jpg', 'make-hair', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (10, 'THX', 'assets/images/gallery/our-gallery-10.jpg', 'spa', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (11, 'THX', 'assets/images/gallery/our-gallery-11.jpg', 'spa', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (12, 'THX', 'assets/images/gallery/our-gallery-12.jpg', 'spa', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (13, 'CYQ', 'assets/images/gallery/our-gallery-13.jpg', 'nail', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (14, 'CYQ', 'assets/images/gallery/our-gallery-14.jpg', 'nail', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
+INSERT INTO `admin_photos` VALUES (15, 'CYQ', 'assets/images/gallery/our-gallery-15.jpg', 'nail', '2019-09-27 09:02:16', '2019-09-27 09:02:16');
 
 -- ----------------------------
 -- Table structure for admin_roles
@@ -82,7 +115,7 @@ CREATE TABLE `admin_setting`  (
 -- ----------------------------
 -- Records of admin_setting
 -- ----------------------------
-INSERT INTO `admin_setting` VALUES (1, '后台管理系统', 'http://127.0.0.1:8000', 'zyxchsjcdd', '深圳市龙华区', '19849975661', '123456789', '78945612', '123456789', '10000', 'Copyright © 2019.ZYX All rights reserved', '2019-09-25 15:52:51', '2019-09-27 00:43:15');
+INSERT INTO `admin_setting` VALUES (1, '后台管理系统', 'http://127.0.0.1:8000/', 'zyxchsjcdd', '深圳市龙华区', '19849975661', '123456789', '78945612', '123456789', '10000', 'Copyright © 2019.ZYX All rights reserved.', '2019-09-25 15:52:51', '2019-09-27 08:27:18');
 
 -- ----------------------------
 -- Table structure for migrations
@@ -93,7 +126,7 @@ CREATE TABLE `migrations`  (
   `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of migrations
@@ -102,6 +135,7 @@ INSERT INTO `migrations` VALUES (1, '2014_10_12_000000_create_users_table', 1);
 INSERT INTO `migrations` VALUES (2, '2019_09_12_035251_create_admin_accounts_table', 2);
 INSERT INTO `migrations` VALUES (3, '2019_09_12_035648_create_admin_roles_table', 3);
 INSERT INTO `migrations` VALUES (4, '2019_09_26_073446_create_admin_setting_table', 4);
+INSERT INTO `migrations` VALUES (5, '2019_09_27_083047_create_admin_photos_table', 5);
 
 -- ----------------------------
 -- Table structure for users
